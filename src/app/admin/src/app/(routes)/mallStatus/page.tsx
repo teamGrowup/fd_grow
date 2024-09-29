121ad557a669f9fa9f403c38a57b43964da220bd
Minhyuk Kim <labcs@naver.com>
2024-09-29 15:40:52 +0900

    styles: admin 판매 현황 페이지 차트 수정

--- src/app/admin/src/app/(routes)/mallStatus/page.tsx
+++ src/app/admin/src/app/(routes)/mallStatus/page.tsx
@@ -3,7 +3,11 @@
 import React from "react";
 import Image from "next/image";
 import Logo from "@/app/admin/src/assets/smallLogo.png";
+
+import { Button } from "@/packages/ui/src";
 import { useRouter } from "next/navigation";
+
+import TickPlacementBars from "../../../components/Chart";
 
 const ShoppingMallStatusPage: React.FC = () => {
   const router = useRouter();
@@ -19,41 +23,40 @@ const ShoppingMallStatusPage: React.FC = () => {
         />
       </div>
       <div className="flex flex-col gap-5 overflow-y-auto h-[calc(100vh-100px)]">
-        {" "}
-        {/* 로고 영역 높이를 고려하여 조정 */}
-        <div className="bg-gray-300 w-[300px] h-[140px] mx-auto translate-y-6 relative">
-          <div>
-            <img src="*" alt="chart" />
-          </div>
-          <div>
-            <h3 className="text-md font-bold absolute bottom-2 left-6">
+        {/* 판매자 판매 현황 섹션 */}
+        <div className="w-4/5 mx-auto translate-y-6 relative">
+          <div className="bg-gray-100 p-4 rounded-lg shadow-md relative">
+            <h3 className="text-md font-bold mb-4 text-center absolute top-2 left-2">
               판매자 판매 현황
             </h3>
+            <TickPlacementBars />
           </div>
         </div>
-        <div className="bg-gray-300 w-[300px] h-[140px] mx-auto translate-y-6 relative">
-          <div>
-            <img src="*" alt="chart" />
-          </div>
-          <div>
-            <h3 className="text-md font-bold absolute bottom-2 left-6">
+
+        {/* 쇼핑몰 영업이익 현황 섹션 */}
+        <div className="w-4/5 mx-auto translate-y-6 relative">
+          <div className="bg-gray-100 p-4 rounded-lg shadow-md relative">
+            <h3 className="text-md font-bold mb-4 text-center absolute top-2 left-2">
               쇼핑몰 영업이익 현황
             </h3>
+            <TickPlacementBars />
           </div>
         </div>
-        <div className="bg-gray-300 w-[300px] h-[140px] mx-auto translate-y-6 relative">
-          <div>
-            <img src="*" alt="chart" />
-          </div>
-          <div>
-            <h3 className="text-md font-bold absolute bottom-2 left-6">
+
+        {/* 쇼핑몰 보유 자산 섹션 */}
+        <div className="w-4/5 mx-auto translate-y-6 relative">
+          <div className="bg-gray-100 p-4 rounded-lg shadow-md relative">
+            <h3 className="text-md font-bold mb-4 text-center absolute top-2 left-2">
               쇼핑몰 보유 자산
             </h3>
+            <TickPlacementBars />
           </div>
         </div>
-        <div className="bg-gray-300 w-[300px] h-[90px] mx-auto translate-y-16 flex justify-center items-center">
-          <div>
-            <p className="text-md font-bold">데이터 필터링</p>
+
+        {/* 데이터 필터링 섹션 */}
+        <div className="w-4/5 h-1/2 rounded-md mx-auto translate-y-16 flex justify-center items-center cursor-pointer">
+          <div className="w-full bg-black rounded-lg text-center">
+            <Button className="text-md font-bold">데이터 필터링</Button>
           </div>
         </div>
       </div>
