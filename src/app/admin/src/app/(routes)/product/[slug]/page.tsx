7a1a92dee72c3331399da5869c80289c51683151
Minhyuk Kim <labcs@naver.com>
2024-10-21 17:29:25 +0900

    styles: admin 상품 상세 페이지 수정

--- src/app/admin/src/app/(routes)/product/[slug]/page.tsx
+++ src/app/admin/src/app/(routes)/product/[slug]/page.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import { Button } from "@/packages/ui/src/index";
-import LogoBar from "@/app/admin/src/components/LogoBar";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import { SlArrowLeft } from "react-icons/sl";
-import { GoHeart } from "react-icons/go";
+import { SlArrowDown } from "react-icons/sl";
+import { SlArrowUp } from "react-icons/sl";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdCheckCircle, MdCancel } from "react-icons/md"; // 체크 및 취소 아이콘 추가
 import Image from "next/image";
 
 import shoesImg from "@/app/admin/src/assets/shoes.jpg";
+import clothesLogo from "@/app/admin/src/assets/adidasLogo.png";
+import backShoes1 from "@/app/admin/src/assets/backShoes1.jpg";
+import backShoes2 from "@/app/admin/src/assets/backShoes2.jpg";
 
 const ProductDetailPage: React.FC = () => {
   const { params } = useParams();
   const [isApproved, setIsApproved] = useState<string | null>(null);
+  const [showMore, setShowMore] = useState<boolean>(false);
+
   const router = useRouter();
 
   useEffect(() => {
@@ -29,42 +36,102 @@ const ProductDetailPage: React.FC = () => {
     router.back();
   };
 
+  const handleHomeClick = () => {
+    router.push("/main");
+  };
+
+  const handleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
+  const arrowComponent = showMore ? <SlArrowUp /> : <SlArrowDown />;
+
   return (
     <>
-      <LogoBar />
       <div className="relative">
         <SlArrowLeft
-          className="absolute m-5 cursor-pointer z-10"
-          size={25} // 아이콘 크기 조정
+          className="absolute cursor-pointer m-5"
+          size={20}
           onClick={handleClick}
         />
+        <IoHomeOutline
+          className="absolute cursor-pointer m-4 right-4"
+          size={26}
+          onClick={handleHomeClick}
+        />
       </div>
-      <div className="relative bg-white w-full mx-auto rounded-lg mt-10 overflow-y-auto">
-        {" "}
-        {/* 비율 기반 크기 조정 */}
-        {/* <h1 className="text-xl font-bold text-black text-center mb-4">
-          상품 {params}에 대한 분석 화면
-        </h1> */}
-        <Image src={shoesImg} alt="shoesImg" className="w-full h-[70vh] mt-4" />
-        <div className="border-b-2 border-gray-200 py-2 flex justify-between">
-          <p className="font-bold text-lg font-sans ml-3">Runner-53A</p>
-          <p className="font-bold text-lg font-sans mr-3">아디다스</p>
-          {/* <div className="flex gap-1 border-2 border-gray-300 rounded-md px-1 justify-center items-center mr-2">
-            <GoHeart />
-            <p>5.3만</p>
-          </div> */}
-        </div>
-        <div className="flex justify-between absolute bottom-4 left-0 right-0 px-4">
-          {isApproved === "null" && (
-            <>
-              <Button className="bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition-colors w-full mr-2">
-                허가
-              </Button>
-              <Button className="bg-red-500 text-white py-2 rounded-lg hover:bg-red-600 transition-colors w-full ml-2">
-                거부
-              </Button>
-            </>
+      <div className="relative bg-white w-full mx-auto rounded-lg mt-10 shadow-md h-screen">
+        {/* 스크롤 가능한 영역 */}
+        <div className="overflow-y-auto h-full">
+          <Image
+            src={shoesImg}
+            alt="shoesImg"
+            className="w-full h-[70vh] mt-4 rounded-t-lg object-cover"
+          />
+
+          {/* 브랜드 및 로고 */}
+          <div className="border-b-2 border-gray-200 py-2 flex items-center">
+            <div className="rounded-full bg-white border-2 border-gray-200 w-12 h-12 flex justify-center items-center ml-4">
+              <Image
+                src={clothesLogo}
+                alt="clothesLogo"
+                className="object-contain"
+              />
+            </div>
+            <p className="font-musinsa font-bold text-md ml-4 text-gray-800">
+              아디다스
+            </p>
+          </div>
+
+          {/* 상품명 및 가격 */}
+          <div className="m-4 flex justify-between">
+            <p className="font-musinsa font-bold text-lg text-gray-900">
+              러닝슈즈 Runner 5A13
+            </p>
+            <p className="font-musinsa font-bold text-lg text-gray-900">
+              129,000원
+            </p>
+          </div>
+
+          <div className="flex justify-center border-2 border-gray-200 py-2 translate-y-2">
+            <button
+              className="flex items-center gap-2 font-musinsa font-bold"
+              onClick={handleShowMore}
+            >
+              상품 정보 더보기 <span>{arrowComponent}</span>
+            </button>
+          </div>
+
+          {showMore && (
+            <div className="flex flex-col mt-4">
+              <Image
+                src={backShoes1}
+                alt="backShoes1"
+                className="object-contain"
+              />
+              <Image
+                src={backShoes2}
+                alt="backShoes2"
+                className="object-contain"
+              />
+            </div>
           )}
+
+          {/* 승인/거부 버튼 */}
+          <div className="flex justify-between px-4 mt-5">
+            {isApproved === "null" && (
+              <>
+                <Button className="text-white py-2 hover:bg-green-700 transition-colors w-full mr-2 shadow-md flex items-center justify-center border-2 border-gray-200">
+                  <MdCheckCircle className="mr-2" />
+                  허가
+                </Button>
+                <Button className="text-white py-2 hover:bg-red-700 transition-colors w-full ml-2 shadow-md flex items-center justify-center border-2 border-gray-200">
+                  <MdCancel className="mr-2" />
+                  거부
+                </Button>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
