7bf1f555a25286f5b94e9bd99d8cecce9a73dcdb
Minhyuk Kim <labcs@naver.com>
2024-10-23 14:22:59 +0900

    feat : 브랜드 별 판매 건 수 리스트 구현

--- src/app/admin/src/app/(routes)/mallStatus/page.tsx
+++ src/app/admin/src/app/(routes)/mallStatus/page.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import LogoBar from "../../../components/LogoBar";
 import DataFilterButton from "../../../components/DataFilterButton";
 import TickPlacementBars from "../../../components/Chart";
+import { DataTableDemo } from "../../../components/DataTable";
 
 type filterCategory = "count" | "money";
 
@@ -53,16 +54,18 @@ const ShoppingMallStatusPage: React.FC = () => {
             </>
           )}
 
-          {category === "count" && ( // 쇼핑몰이니까 전체 판매 건수 현황으로 시장 거래가 활발한지 정도만 체크, 판매자별 건수 현황 말고 차라리 판매자 파트에서 개개인 만이 판매 견수 현황을 볼 수 있게 구현하는건 어떤지?
+          {category === "count" && (
             <>
-              {/* 쇼핑몰 영업이익 현황 섹션 */}
-              <div className="w-4/5 mx-auto translate-y-6 relative">
+              <div className="w-full mx-auto translate-y-6 relative">
                 <div className="bg-gray-100 p-4 rounded-lg shadow-md relative">
                   <h3 className="text-md font-bold mb-4 text-center absolute top-2 left-2">
                     쇼핑몰 전체 영업 건수 현황
                   </h3>
                   <TickPlacementBars unit="count" />
                 </div>
+              </div>
+              <div className="mt-8 bg-gray-100">
+                <DataTableDemo />
               </div>
             </>
           )}
---
+++ src/app/admin/src/components/DataTable.tsx
@@ -0,0 +1,246 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+
+import clothesLogo from "@/app/admin/src/assets/adidasLogo.png";
+
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Input } from "./ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+
+const data: Payment[] = [
+  {
+    id: "m5gr84i9",
+    amount: 316,
+    brand: "아디다스",
+  },
+  {
+    id: "3u1reuv4",
+    amount: 242,
+    brand: "나이키",
+  },
+  {
+    id: "derv1ws0",
+    amount: 837,
+    brand: "푸마",
+  },
+  {
+    id: "5kma53ae",
+    amount: 874,
+    brand: "뉴발란스",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    brand: "미즈노",
+  },
+];
+
+export type Payment = {
+  id: string;
+  amount: number;
+  brand: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "brand",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          브랜드
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="lowercase ml-4 flex items-center gap-2">
+        <span>
+          <div className="rounded-full bg-white border-2 border-gray-200 w-8 h-8 flex justify-center items-center">
+            <Image
+              src={clothesLogo}
+              alt="clothesLogo"
+              className="object-contain"
+            />
+          </div>
+        </span>{" "}
+        {row.getValue("brand")}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => {
+      return (
+        <div className="flex justify-end">
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            판매 건수
+            <ArrowUpDown />
+          </Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+
+      return <div className="text-right font-medium mr-10">{amount}</div>;
+    },
+  },
+];
+
+export function DataTableDemo() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="브랜드를 검색하세요..."
+          value={(table.getColumn("brand")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("brand")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              카테고리 <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
